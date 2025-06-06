b3852c0761c0e21e52cda6fb63d70aa6656596a0
sway-sea <itraynes@gmail.com>
2025-06-06 11:14:39 +0200

    added player + movement

--- main.py
+++ main.py
@@ -3,16 +3,24 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
-    pygame.init
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0    
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
     while True:
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = (clock.tick(60) / 1000)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
